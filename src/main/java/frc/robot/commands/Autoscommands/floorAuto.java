31f2fa7011af6c7ab3ba2ea20af22b79a2231a80
Elliot Holmes <elliot.b.holmes@gmail.com>
2024-04-06 09:01:15 -0400

    stuff and intake has a timer

---
+++ src/main/java/frc/robot/commands/Autoscommands/floorAuto.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Autoscommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.PivotIntake;
+import edu.wpi.first.wpilibj.Timer;
+
+public class floorAuto extends Command {
+  private PivotIntake intakePivot;
+  private Intake intake;
+  private Timer time;
+
+  public floorAuto(Intake intake, PivotIntake intakePivot) {
+    this.intake = intake;
+    this.intakePivot = intakePivot;
+    addRequirements(intakePivot, intake);
+
+  }
+
+  @Override
+  public void initialize() {
+    intake.setRollerPower(Constants.IntakeConstants.ROLLER_SPEED);
+    intakePivot.setHingePower(Constants.IntakeConstants.PIVOT_OUT_SPEED);
+    time.start();
+  }
+
+  @Override
+  public void execute() {
+    if (intakePivot.getPosition() > Constants.IntakeConstants.DROPPED) {
+      intakePivot.setHingePower(0.0);
+    }
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    intake.setRollerPower(0.0);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return intake.switchPressed() || time.hasElapsed(5) ;
+  }
+}
